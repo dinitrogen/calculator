71681ca9757f25298ebddad085b69d393ee0543e
dinitrogen <noshowdon@gmail.com>
2021-06-04 15:29:20 -0400

    Number and clear buttons functioning

--- script.js
+++ script.js
@@ -7,6 +7,39 @@ import {
     factorial
   } from './calculator.js';
 
-let container = document.querySelector("#container");
+const calcDisplay = document.querySelector("#calcDisplay");
+
+    
+let displayText = "0";
+calcDisplay.textContent = displayText;
+
+// Clear button press
+const buttonClear = document.querySelector("#buttonClear");
+buttonClear.addEventListener('click', () => {
+    displayText = "0";
+    calcDisplay.textContent = displayText;
+});
+
+// Number buttons press
+const numButtons = document.querySelectorAll(".numButton");
+numButtons.forEach((numButton) => {
+    
+    numButton.addEventListener('click', () => {
+        updateDisplay(numButton.textContent);
+    });
+});
+
+// TODO Negative press
+// TODO Decimal point press
+// TODO Operations press
 
 
+// Numbers update display
+function updateDisplay(buttonInput) {
+    if (displayText === "0") {
+        displayText = `${buttonInput}`;
+    } else {
+    displayText = `${displayText}${buttonInput}`;
+    }
+    calcDisplay.textContent = displayText;
+}
